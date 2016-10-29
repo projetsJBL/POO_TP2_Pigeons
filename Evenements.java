9ac07482166e7041570838c7a275f254a5abf219
projetsJBL <projetsjbl@gmail.com>
2016-10-29 13:09:54 -0400

    Update Evenements.java

--- Evenements.java
+++ Evenements.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 import javax.swing.JFrame;
 
 /**
- * Classe Evenements Implement les rÈactions des pigeons selon leur etat
+ * Classe Evenements Implement les r√©actions des pigeons selon leur etat
  * 
  * @author JB maj 29/10/2016
  */
@@ -18,26 +18,29 @@ public class Evenements extends JFrame {
 
 	// Constructeur
 	public Evenements() {
-		super("Jeu des Pigeons affamÈs");
-		setExtendedState(JFrame.MAXIMIZED_BOTH); // totalitÈ de l'Ècran
+		super("Jeu des Pigeons affam√©s");
+		
+		// totalit√© de l'√©cran
+		// A modifi√© selon la taille de l'image!
+		setExtendedState(JFrame.MAXIMIZED_BOTH); 
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		pigeons = new ArrayList<Pigeon>();
 		nourritures = new ArrayList<Nourriture>();
 	}
 
-	// Ajout d'un pigeon dans la liste appropriÈe
+	// Ajout d'un pigeon dans la liste appropri√©e
 	public void addPigeon(Pigeon pigeon) {
 		pigeons.add(pigeon);
 
 	}
 
-	// Ajout de nourritures dans la liste appropriÈe
+	// Ajout de nourritures dans la liste appropri√©e
 	public void addNourriture(Nourriture nourriture) {
 		nourritures.add(nourriture);
 	}
 
-	// Retirer de la nourriture quand elle est mangÈe 
-	// retire le dernier ÈlÈment ajouter
+	// Retirer de la nourriture quand elle est mang√©e 
+	// retire le dernier √©l√©ment ajouter
 	public void RemoveNourriture() {
 		int i = nourritures.size();
 		nourritures.remove(i);
