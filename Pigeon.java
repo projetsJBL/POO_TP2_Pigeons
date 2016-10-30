ca3bad138fa3746fa93853d855ccb25a90517870
projetsJBL <projetsjbl@gmail.com>
2016-10-30 19:16:24 -0400

    Add files via upload

--- Pigeon.java
+++ Pigeon.java
@@ -15,19 +15,24 @@ public class Pigeon extends ElementDynamique{
 	//static int velocity = 10;
 	Etat etat;
 	static int mangent;
+	static int fatigue;
 	static ArrayList<Nourriture> nourriture = new ArrayList<Nourriture>();
 	static ArrayList<Pigeon> pigeons = new ArrayList<Pigeon>();
 	
 	String nom;
-	
+	boolean devantNourriture;
 	
 	public Pigeon(String imageName){
+		//graphique
 		setImageName(imageName);
 		JLabel label = new JLabel();
 		label.setIcon(new ImageIcon(imageName));
 		setLabel(label);
+		
+		//caracteristiques
 		etat = Etat.WAITING; //le pigeon est en attente par defaut
 		pigeons.add(this);
+		devantNourriture = false;
 		
 		//pour tests
 		nom = "";		
@@ -52,12 +57,20 @@ public class Pigeon extends ElementDynamique{
 		return pigeons;
 	}
 	
+	public boolean getDevantNourriture(){
+		return devantNourriture;
+	}
+	
 	public void setEtat(Etat e){
 		etat = e;
 	}
 	
 	public void setNom(String new_nom){
 		nom = new_nom;
+	}
+	
+	public void setDevantNourriture(boolean b){
+		devantNourriture = b;
 	}
 	
 	
@@ -72,8 +85,9 @@ public class Pigeon extends ElementDynamique{
 	 * retire la nourriture la plus fraiche
 	 */
 	public static void removeNourriture(Nourriture n){
-		if(nourriture.size() > 0)
+		if(!nourriture.isEmpty()){
 			nourriture.remove(n);
+		}
 		else
 			System.out.println("nourriture vide");
 	}
@@ -82,23 +96,49 @@ public class Pigeon extends ElementDynamique{
 	 * Determine l'etat du pigeon selon la situation
 	 */
 	public void determinerEtat(){
-		if(nourriture.isEmpty()) //s'il n'y a pas de nourriture
+		
+		//il n'y a plus de nourriture
+		if(nourriture.isEmpty() && fatigue < 20)
 			this.setEtat(Etat.WAITING);
-		else if (!nourriture.isEmpty() && !this.getEtat().equals(Etat.MOVING)) //si il y a de la nourriture et qu'il n'a pas encore bouge
-			this.setEtat(Etat.MOVING);
-		else if(!nourriture.isEmpty() && this.getEtat().equals(Etat.MOVING)) //si il arrive sur de la nourriture qui n'est pas mangee
+		
+		//il y a de la nourriture
+		else if (!nourriture.isEmpty() && !devantNourriture){ //si il y a de la nourriture et qu'il n'a pas encore bouge		
+
+			Nourriture n = nourriture.get(nourriture.size()-1);
+			double[] vecteurPN = {n.getX() - this.getX(), n.getY() - this.getY()}; //vecteur pigeon-nourriture
+			double normePN = Math.sqrt((Math.pow(vecteurPN[0], 2) + Math.pow(vecteurPN[1], 2))); //distance pigeon-nourriture
+			
+			if(normePN > 1){
+				this.setEtat(Etat.MOVING);
+				fatigue = 0; //on remet leur fatigue a 0 pour eviter qu'il s'endorment juste apres
+			}
+			else{
+				this.setDevantNourriture(true);
+				System.out.println(this.getDevantNourriture());
+			}
+		}
+		
+		else if(!nourriture.isEmpty() && this.getEtat().equals(Etat.MOVING)){ //si il arrive sur de la nourriture qui n'est pas mangee
+			System.out.println(this.getNom() + " ARRIVED");
+			System.out.println("Position arrivee " + this.getNom() + ": " + this.getX() + "," + this.getY());
 			this.setEtat(Etat.EATING);
+		}
+		else if(nourriture.isEmpty() && fatigue >= 20)
+			this.setEtat(Etat.SLEEPING);
 	}
 	
 	/*
 	 * Deplacement du pigeon vers une position donnee
 	 */
-	public void seDeplacer(double xn, double yn){
-		double[] vecteurPN = {xn - this.getX(), yn - this.getY()}; //vecteur pigeon-nourriture
-		double normePN = Math.sqrt((Math.pow(vecteurPN[0], 2) + Math.pow(vecteurPN[1], 2))); //distance pigeon-nourriture
-		double[] PNnormalise = {vecteurPN[0] / normePN, vecteurPN[1] / normePN}; //Vecteur normalise pour un deplacement a vitesse uniforme
-		
-		while(normePN > 1){ //tant qu'il n'a pas atteint la nourriture (approximatif)
+	public void seDeplacer(){
+		double normePN = 2;
+		if(normePN >1 ){
+			Nourriture n = nourriture.get(nourriture.size()-1);
+			double[] vecteurPN = {n.getX() - this.getX(), n.getY() - this.getY()}; //vecteur pigeon-nourriture
+			normePN = Math.sqrt((Math.pow(vecteurPN[0], 2) + Math.pow(vecteurPN[1], 2))); //distance pigeon-nourriture
+			double[] PNnormalise = {vecteurPN[0] / normePN, vecteurPN[1] / normePN}; //Vecteur normalise pour un deplacement a vitesse uniforme
+			
+			//tant qu'il n'a pas atteint la destination (approximatif) 
 			System.out.println("position arrondie pigeon " + this.getNom() + ": " + (int)this.getX() + " , " + (int)this.getY());
 			
 			this.setX(this.getX() + PNnormalise[0]);
@@ -106,21 +146,34 @@ public class Pigeon extends ElementDynamique{
 			this.getLabel().setLocation((int)this.getX(), (int)this.getY()); //on met a jour le sprite
 			
 			//on recalcule la position du pigeon par rapport a la nourriture
-			vecteurPN[0] = xn - this.getX();
-			vecteurPN[1] = yn - this.getY();
+			vecteurPN[0] = n.getX() - this.getX();
+			vecteurPN[1] = n.getY() - this.getY();
 			normePN = Math.sqrt((Math.pow(vecteurPN[0], 2) + Math.pow(vecteurPN[1], 2))); //distance pigeon-nourriture
-		}		
+		}			
 	}
 	
 	/*
-	 * Mange et retire la nourriture la plus fraiche de la liste nourriture
+	 * Mange et retire la nourriture la plus fraiche de la liste nourriture si tous les pigeons l'ont mange
 	 */
 	public void manger(Nourriture n){
 		mangent++;		
-		if(mangent == pigeons.size()){
+		
+		if(mangent == pigeons.size()){			
 			removeNourriture(n);
 			mangent = 0;
 		}
+	}
+	
+	public void manger2(Nourriture n){
+		removeNourriture(n);
+	}
+	
+	/*
+	 * Les pigeons fuient
+	 */
+	public void fuir(){
+		double x = Math.random()*(1000) * (Math.random() < 0.5 ? -1 : 1); //le premier terme donne un alea entre 0 et mille, le deuxieme un facteur -1 ou 1
+		double y = Math.random()*(1000) * (Math.random() < 0.5 ? -1 : 1);
 	}
 	
 	/**
@@ -129,27 +182,21 @@ public class Pigeon extends ElementDynamique{
 	
 	@Override
 	public void run(){
-		for(int i = 0; i < 5; i++){
+		for(int i = 0; i < 100; i++){
 			this.determinerEtat();
-				
 			
 			switch(this.getEtat()){
 			case WAITING: 
 				try {
+					fatigue++;
 					Thread.sleep(1000);
 					System.out.println(this.getNom() + " WAITING...");
 				} catch (InterruptedException e1) {e1.printStackTrace();}
 				break;
 			case MOVING:
-				try {
-					Nourriture n = nourriture.get(nourriture.size()-1);
-					double xn = n.getX(); double yn = n.getY(); //on recupere sa position
-					System.out.println("Position depart " + this.getNom() + ": " + this.getX() + "," + this.getY());
-					System.out.println(this.getNom() + " MOVING...");
-					this.seDeplacer(xn, yn);			
-					Thread.sleep(1000);
-					System.out.println(this.getNom() + " MOVED");
-					System.out.println("Position arrivee " + this.getNom() + ": " + this.getX() + "," + this.getY());
+				try {		
+					this.seDeplacer();					
+					Thread.sleep(500);
 					} catch (InterruptedException e) {e.printStackTrace();}
 				break;
 			case EATING:
@@ -157,9 +204,16 @@ public class Pigeon extends ElementDynamique{
 					Nourriture n = nourriture.get(nourriture.size()-1);
 					System.out.println(this.getNom() + " EATING...");
 					this.manger(n);
+					Evenements.supprimerNourriture(n);
 					Thread.sleep(1000);
 					System.out.println(this.getNom() + " ATE");
 					System.out.println("Il reste " + nourriture.size() + " nourritures");
+				}catch (InterruptedException e) {e.printStackTrace();}
+				break;
+			case SLEEPING:
+				try{
+					System.out.println(this.getNom() + " SLEEPING...");
+					Thread.sleep(1000);
 				}catch (InterruptedException e) {e.printStackTrace();}
 				break;
 			default:
