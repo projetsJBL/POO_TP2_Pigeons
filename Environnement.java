5f0dd398fb9f08ee239f6715370d26f1f57e07cf
projetsJBL <projetsjbl@gmail.com>
2016-10-30 14:04:03 -0400

    Update Environnement.java

--- Environnement.java
+++ Environnement.java
@@ -6,13 +6,19 @@
  * maj 29/10/2016
  */
 
-import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
 import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class Environnement extends JPanel {
+public class Environnement extends JPanel implements MouseListener, MouseMotionListener {
 	protected ImageIcon image = new ImageIcon("environnement.jpg");
+	private int xclic, yclic;
 	/**
 	 * Version par défault
 	 */
@@ -23,6 +29,7 @@ public class Environnement extends JPanel {
 		super.setOpaque(false);
 		g.drawImage(image.getImage(), 0, 0, null);
 		super.paintComponent(g);
+
 	}
 
 	public ImageIcon getImage() {
@@ -37,4 +44,77 @@ public class Environnement extends JPanel {
 		return taille;
 
 	}
+
+	public int getXclic() {
+		return xclic;
+	}
+
+	public int getYclic() {
+		return yclic;
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		// TODO Auto-generated method stub
+		xclic = e.getX();
+		yclic = e.getY();
+		System.out.println(xclic + ";" + yclic);
+
+		Nourriture n = new Nourriture("nourriture.png");
+
+		// ajout de nourriture dans la liste nourriture de pigeon
+		n.setX((double) xclic);
+		n.setY((double) yclic);
+		Pigeon.addNourriture(n);
+		for (int i = 0; i < Pigeon.getNourriture().size(); i++){
+		System.out.println(
+					"nourriture " + Pigeon.getNourriture().get(i).getX() + ";" + Pigeon.getNourriture().get(i).getX());}
+
+		// Ajout de l'image dans l'interface graphique
+		n.getLabel().setLocation(xclic, yclic);
+		this.add(n.getLabel());
+
+		Main.evt.addNourriture(n);
+		System.out.println("nourriture image " + n.getLabel().getLocation());
+
+		Main.evt.startNourriture();
+
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseDragged(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void mouseMoved(MouseEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
 }
