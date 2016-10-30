3822254db33bc12d5e0f2dd3e3f3c880ce017dbf
projetsJBL <projetsjbl@gmail.com>
2016-10-30 14:04:29 -0400

    Update Evenements.java

--- Evenements.java
+++ Evenements.java
@@ -1,4 +1,7 @@
+import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
 import javax.swing.ImageIcon;
@@ -11,27 +14,31 @@ import javax.swing.JFrame;
  */
 
 public class Evenements extends JFrame {
-
+	private static final long serialVersionUID = 1L;
 	/**
 	 * version par default
 	 */
-	private static final long serialVersionUID = 1L;
+
 	private ArrayList<Pigeon> pigeons;
 	private ArrayList<Nourriture> nourritures;
+	Environnement ev;
 
 	// Constructeur
 	public Evenements() {
 		super("Jeu des Pigeons affamés");
-		
-		//on récupère la taille de l'image
-		Environnement ev = new Environnement();
+
+		// on récupère la taille de l'image
+		ev = new Environnement();
 		ImageIcon image = ev.getImage();
 
-		
-		//on adapte la fenêtre à la taille de l'image de fond
+		// on adapte la fenêtre à la taille de l'image de fond
 		this.setSize(image.getIconWidth(), image.getIconHeight());
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
+
+		add(ev);
+		addMouseListener(ev);
+		addMouseMotionListener(ev);
+
 		// Création des listes nécessaires
 		pigeons = new ArrayList<Pigeon>();
 		nourritures = new ArrayList<Nourriture>();
@@ -43,30 +50,46 @@ public class Evenements extends JFrame {
 
 	}
 
-	// Ajout de nourritures dans la liste appropriée
-	public void addNourriture(Nourriture nourriture) {
-		nourritures.add(nourriture);
-	}
-
 	// Retirer de la nourriture quand elle est mangée
 	// retire le dernier élément ajouter
 	public void RemoveNourriture() {
 		int i = nourritures.size();
-		nourritures.remove(i);
+		nourritures.remove(i - 1);
 	}
+	// gestion des réactions des pigeons
 
-	//gestion des réactions des pigeons
-	public void start() {
-		Environnement evt = new Environnement();
+	public void startPigeon() {
+		// Ajout des pigeons dans la fenêtre de jeux
 
 		for (Pigeon pigeon : pigeons) {
-			evt.add(pigeon.getLabel());
+			ev.add(pigeon.getLabel());
+			add(ev);
 		}
-
-		add(evt);
 
 		setVisible(true);
 
+	}
+
+	public void startNourriture() {
+		for (Nourriture nourriture : nourritures) {
+			ev.add(nourriture.getLabel());
+
+		}
+		add(ev);
+		setVisible(true);
+
+		for (Nourriture nourriture : nourritures) {
+			nourriture.getLabel().setLocation((int) nourriture.getX(), (int) nourriture.getY());
+		}
+		for (Pigeon pigeon : pigeons) {
+
+			pigeon.start();
+		}
+	}
+
+	public void addNourriture(Nourriture n) {
+		// TODO Auto-generated method stub
+		nourritures.add(n);
 	}
 
 }
