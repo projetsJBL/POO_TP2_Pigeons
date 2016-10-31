b5c5453930e9275cb97cd907e8f130f3d28b11d8
projetsJBL <projetsjbl@gmail.com>
2016-10-30 22:05:48 -0400

    Update Pigeon.java

--- Pigeon.java
+++ Pigeon.java
@@ -12,7 +12,7 @@ import javax.swing.JLabel;
 
 public class Pigeon extends ElementDynamique{
 	
-	//static int velocity = 10;
+	int velocity = 1;
 	Etat etat;
 	static int mangent;
 	static int fatigue;
@@ -21,6 +21,7 @@ public class Pigeon extends ElementDynamique{
 	
 	String nom;
 	boolean devantNourriture;
+	boolean fuit;
 	
 	public Pigeon(String imageName){
 		//graphique
@@ -33,6 +34,7 @@ public class Pigeon extends ElementDynamique{
 		etat = Etat.WAITING; //le pigeon est en attente par defaut
 		pigeons.add(this);
 		devantNourriture = false;
+		fuit = false;
 		
 		//pour tests
 		nom = "";		
@@ -61,6 +63,10 @@ public class Pigeon extends ElementDynamique{
 		return devantNourriture;
 	}
 	
+	public boolean getFuit(){
+		return fuit;
+	}
+	
 	public void setEtat(Etat e){
 		etat = e;
 	}
@@ -71,6 +77,14 @@ public class Pigeon extends ElementDynamique{
 	
 	public void setDevantNourriture(boolean b){
 		devantNourriture = b;
+	}
+	
+	public void setVelocity(int v){
+		velocity = v;
+	}
+	
+	public void setFuit(boolean b){
+		fuit = b;
 	}
 	
 	
@@ -95,19 +109,21 @@ public class Pigeon extends ElementDynamique{
 	/*
 	 * Determine l'etat du pigeon selon la situation
 	 */
-	public void determinerEtat(){
-		
+	public void determinerEtat(){	
+		System.out.println("Fuite: " + this.getFuit());
+		if(this.getFuit()){
+			this.setEtat(Etat.RUNNING);
+		}
+		else{
 		//il n'y a plus de nourriture
 		if(nourriture.isEmpty() && fatigue < 20)
 			this.setEtat(Etat.WAITING);
 		
 		//il y a de la nourriture
-		else if (!nourriture.isEmpty() && !this.devantNourriture){ //si il y a de la nourriture et qu'il n'a pas encore bouge		
-
+		else if (!nourriture.isEmpty() && !this.devantNourriture){ //si il y a de la nourriture et qu'il n'a pas encore bouge
 			Nourriture n = nourriture.get(nourriture.size()-1);
 			double[] vecteurPN = {n.getX() - this.getX(), n.getY() - this.getY()}; //vecteur pigeon-nourriture
-			double normePN = Math.sqrt((Math.pow(vecteurPN[0], 2) + Math.pow(vecteurPN[1], 2))); //distance pigeon-nourriture
-			
+			double normePN = Math.sqrt((Math.pow(vecteurPN[0], 2) + Math.pow(vecteurPN[1], 2))); //distance pigeon-nourriture			
 			if(normePN > 1){
 				this.setEtat(Etat.MOVING);
 				fatigue = 0; //on remet leur fatigue a 0 pour eviter qu'il s'endorment juste apres
@@ -128,6 +144,7 @@ public class Pigeon extends ElementDynamique{
 		//dormir
 		else if(nourriture.isEmpty() && fatigue >= 20)
 			this.setEtat(Etat.SLEEPING);
+		}
 	}
 	
 	/*
@@ -135,8 +152,8 @@ public class Pigeon extends ElementDynamique{
 	 */
 	public void seDeplacer(){
 		double normePN = 2;
-		if(normePN >1 ){
-			Nourriture n = nourriture.get(nourriture.size()-1);
+		Nourriture n = nourriture.get(nourriture.size()-1);
+		if(normePN > 1 ){			
 			double[] vecteurPN = {n.getX() - this.getX(), n.getY() - this.getY()}; //vecteur pigeon-nourriture
 			normePN = Math.sqrt((Math.pow(vecteurPN[0], 2) + Math.pow(vecteurPN[1], 2))); //distance pigeon-nourriture
 			double[] PNnormalise = {vecteurPN[0] / normePN, vecteurPN[1] / normePN}; //Vecteur normalise pour un deplacement a vitesse uniforme
@@ -165,9 +182,28 @@ public class Pigeon extends ElementDynamique{
 	/*
 	 * Les pigeons fuient
 	 */
-	public void fuir(){
-		double x = Math.random()*(1000) * (Math.random() < 0.5 ? -1 : 1); //le premier terme donne un alea entre 0 et mille, le deuxieme un facteur -1 ou 1
-		double y = Math.random()*(1000) * (Math.random() < 0.5 ? -1 : 1);
+	public void fuir(double x, double y){
+		this.setVelocity(750); //vitesse augmentee
+		//generation d'un point aleatoire
+		
+		double[] vecteurFuite = {x - this.getX(), y - this.getY()};
+		double normeVecteurFuite = Math.sqrt((Math.pow(vecteurFuite[0], 2) + Math.pow(vecteurFuite[1], 2))); //distance pigeon-nourriture
+		double[] vecteurFuiteNormalise = {vecteurFuite[0] / normeVecteurFuite, vecteurFuite[1] / normeVecteurFuite}; //Vecteur normalise pour un deplacement a vitesse uniforme
+		
+		//fuite
+		this.setX(this.getX() + vecteurFuiteNormalise[0]);
+		this.setY(this.getY() + vecteurFuiteNormalise[1]);
+		this.getLabel().setLocation((int)this.getX(), (int)this.getY()); //on met a jour le sprite
+		
+		//on recalcule la position du pigeon par rapport a la nourriture
+		vecteurFuite[0] = x - this.getX();
+		vecteurFuite[1] = y - this.getY();
+		normeVecteurFuite = Math.sqrt((Math.pow(vecteurFuite[0], 2) + Math.pow(vecteurFuite[1], 2))); //distance pigeon-nourriture
+		
+		if(normeVecteurFuite <= 1){
+			this.setFuit(false);
+			this.setVelocity(100 + (int)(Math.random()*(110 - 90) + 90));
+		}
 	}
 	
 	/**
@@ -176,8 +212,15 @@ public class Pigeon extends ElementDynamique{
 	
 	@Override
 	public void run(){
-		for(int i = 0; i < 100; i++){
+		while(true){
 			this.determinerEtat();
+			this.setFuit(Environnement.getPeur());
+			
+			int[] pointFuite = new int[2];
+			if(this.getFuit())
+			{
+				pointFuite = Environnement.getpointFuite();
+			}
 			
 			switch(this.getEtat()){
 			case WAITING: 
@@ -190,7 +233,7 @@ public class Pigeon extends ElementDynamique{
 			case MOVING:
 				try {		
 					this.seDeplacer();					
-					Thread.sleep(500);
+					Thread.sleep(1000/velocity);
 					} catch (InterruptedException e) {e.printStackTrace();}
 				break;
 			case EATING:
@@ -199,7 +242,7 @@ public class Pigeon extends ElementDynamique{
 					System.out.println(this.getNom() + " EATING...");
 					this.manger(n);
 					Evenements.supprimerNourriture(n);
-					Thread.sleep(1000);
+					Thread.sleep(1000/velocity);
 					this.setDevantNourriture(false);
 					System.out.println(this.getNom() + " ATE");
 					System.out.println("Il reste " + nourriture.size() + " nourritures");
@@ -211,6 +254,12 @@ public class Pigeon extends ElementDynamique{
 					Thread.sleep(1000);
 				}catch (InterruptedException e) {e.printStackTrace();}
 				break;
+			case RUNNING:
+				try{
+					System.out.println(this.getNom() + " is RUNNING");
+					this.fuir((double)pointFuite[0],(double)pointFuite[1]);
+					Thread.sleep(1000/velocity);
+				}catch (InterruptedException e) {e.printStackTrace();}
 			default:
 					System.out.println("Default case in run method");
 					break;
